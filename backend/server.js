04008938bcd8e406b078fcf4286d67749071d25a
nelson <chinwubanelson@gmail.com>
2025-04-05 09:04:34 +0100

    deployed repo

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import "dotenv/config";
 import authRouter from "./routes/authRoutes.js";
 import connectDB from "./configs/mongodb.js";
@@ -9,6 +10,7 @@ import userRouter from "./routes/userRoutes.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // To parse incoming requests with JSON payload from req.body
 app.use(express.json());
@@ -31,10 +33,11 @@ connectDB();
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/users", userRouter);
-
-app.get("/", (req, res) => {
-  res.send("API IS WORKING");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
+
 server.listen(PORT, () => {
   console.log("Server Running on Port:", PORT);
 });
